e5812f3b4f73bdc4c7f0ad586dfcfa03073852fd
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-08-22 10:29:39 -0500

    addBearInfo adds objects to the array

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
-import { addBearForm } from './components/bearForm.js';
+import { addBearForm, addBearInfo } from './components/bearForm.js';
 // import { addBearCard } from './components/river.js';
 
 const init = () => {
     addBearForm();
+    addBearInfo();
 };
 
 init();
