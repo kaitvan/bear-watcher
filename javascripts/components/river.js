f8311c7ffaac9120e5e48aa91b50e20df5455415
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-08-24 15:36:50 -0500

    updated styling with font, colors, and spacing

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -2,7 +2,7 @@ const addBearCard = (array) => {
     $('#bear-cards').html('');
     array.forEach((bear) => {
         $('#bear-cards').append(`
-        <div class="card" style="width: 18rem;">
+        <div class="card m-4" style="width: 18rem;">
             <img class="card-img-top" src="${bear.bearImage}" alt="${bear.bearName}">
             <div class="card-body">
                 <p class="card-text">${bear.bearName}</p>
